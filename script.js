1aa57d8a2cc4476df6208cc544d377bc0b76e245
chandan kumar <chandan.kumar@ahom.tech>
2024-05-30 15:11:28 +0530

    changes in js and css file

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function fahrenheit() {
+
+
 	let cel = document.getElementById('cel').value;
-	// console.log(cel);
-	let fah = document.getElementById('fah').value;
-	// console.log(fah);
-	fah = (cel * 9/5) + 32;
-	document.getElementById('fah').value = isDecimal(fah) ? fah.toFixed(1) : fah;
-	// console.log(document.getElementById('fah').value);
+	if (cel == ''){
+		document.getElementById('fah').value='';
+	}else{
+		// console.log(cel);
+		let fah = document.getElementById('fah').value;
+		fah = (cel * 9/5) + 32;
+		document.getElementById('fah').value = isDecimal(fah) ? fah.toFixed(1) : fah;
+
+	}
 
 }
 
 function celsius() {
 	let fah = document.getElementById('fah').value;
-	// console.log(fah);
-	let cel = document.getElementById('cel').value;
-	cel = (fah - 32) * 5/9;
-	document.getElementById('cel').value = isDecimal(cel) ? cel.toFixed(4) : cel;
-
+	if (fah == ''){
+		document.getElementById('cel').value = '';
+	}else{
+		let cel = document.getElementById('cel').value;
+		cel = (fah - 32) * 5/9;
+		document.getElementById('cel').value = isDecimal(cel) ? cel.toFixed(4) : cel;
+	}
 
 }
 
